bd93a84ff41f5a46c6b24dd1a007dcc426cca304
youssie12 <546827@student.fontys.nl>
2025-09-17 01:04:02 +0200

    fixing recursicve

--- infrastructure/envs/dev/backend.tf
+++ infrastructure/envs/dev/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   required_version = ">= 1.3.0"
   backend "s3" {
-    bucket         = "coreassig1-terraform-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
+    bucket  = "coreassig1-terraform-state-bucket"
+    key     = "terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
   }
 }
-
