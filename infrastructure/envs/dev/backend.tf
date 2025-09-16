5eb57fab0eb2b02c6aba9a952efe6bd702a5ba6b
youssie12 <546827@student.fontys.nl>
2025-09-17 00:56:17 +0200

    s3 backend key

--- infrastructure/envs/dev/backend.tf
+++ infrastructure/envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
   backend "s3" {
     bucket         = "coreassig1-terraform-state-bucket"
-    key            = "myproject/prod/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
   }
